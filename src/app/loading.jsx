474ff68a7ce95ed77e296912cd5668dfdb8d0716
Patrick Gip <gippatrick@gmail.com>
2023-03-19 18:52:35 -0700

    Added loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+function loading() {
+  return (
+    <div className="flex justify-center">
+      <img className="h-96" src="spinner.svg" alt="Loading..." />
+    </div>
+  );
+}
+
+export default loading;
